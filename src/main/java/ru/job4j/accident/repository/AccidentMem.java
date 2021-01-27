3e5fe9600e066d06a297eb0886526f99f497dfb0
Sergey Morozov <moroz.future@gmail.com>
2021-01-27 20:35:42 +0400

    Исправление: IndexControl. Таблица и вид.

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -6,16 +6,16 @@ import ru.job4j.accident.model.Accident;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class AccidentMem {
     private static final AtomicInteger ACCIDENT_ID = new AtomicInteger(0);
 
-    private Map<Integer, Accident> accidents;
+    private final Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
 
-    public AccidentMem(Map<Integer, Accident> accidents) {
-        this.accidents = accidents;
+    {
         accidents.put(ACCIDENT_ID.incrementAndGet(), new Accident(1, "one", "description 1", "address 1"));
         accidents.put(ACCIDENT_ID.incrementAndGet(), new Accident(2, "two", "description 2", "address 2"));
         accidents.put(ACCIDENT_ID.incrementAndGet(), new Accident(3, "three", "description 3", "address 3"));
@@ -24,6 +24,6 @@ public class AccidentMem {
     }
 
     public List<Accident> getAccidents() {
-        return accidents != null ? new ArrayList<>(accidents.values()) : new ArrayList<>();
+        return new ArrayList<>(accidents.values());
     }
 }
